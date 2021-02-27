05cd13d6b822aa0bec2c3f3e0c48c0ad786a990a
Nawaphong Oncharun <nawaphong.on.com>
2021-02-28 01:41:31 +0700

    first commit

---
+++ app2.js
@@ -0,0 +1,48 @@
+//EP 2
+//ตอบกลับ message ธรรมดา
+
+const express = require('express');
+const line = require('@line/bot-sdk');
+const settings = require('./config.json');
+
+const app = express();
+
+console.log(settings);
+
+const config = {
+    channelAccessToken: settings.line.channelAccessToken,
+    channelSecret: settings.line.channelSecret
+};
+
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
+
+function handleEvent(event) {
+
+    console.log(event);
+    if (event.type === 'message' && event.message.type === 'text') {
+        handleMessageEvent(event);
+    } else {
+        return Promise.resolve(null);
+    }
+}
+
+function handleMessageEvent(event) {
+    var msg = {
+        type: 'text',
+        text: 'สวัสดีครัช'
+    };
+
+    return client.replyMessage(event.replyToken, msg);
+}
+
+app.set('port', (settings.port));
+
+app.listen(app.get('port'), function () {
+    console.log('run at port', app.get('port'));
+});
